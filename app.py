b46804f46fcfabde4f21f70b7c51924da57c9725
alanpoire <eloyaranda.ap@gmail.com>
2024-10-10 21:42:30 -0600

    Agregado el encabezado en la aplicación web

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import plotly.express as px
 import streamlit as st
 import pandas as pd
 
+# agregar encabezado de la aplicación
+st.header("Aplicación Web de Visualización de Datos de Vehiculos")
+
 car_data = pd.read_csv('vehicles_us (1).csv')  # leer los datos
 hist_button = st.button('Construir histograma')  # crear un botón
 
@@ -13,6 +16,7 @@ if hist_button:  # al hacer clic en el botón
     fig = px.histogram(car_data, x='odometer')
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
 
 # crear segundo botón para gráfico de dispersión
 disp_button = st.button('Construir gráfico dispersión')
